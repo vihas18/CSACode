eea956b5ff308667df1c93dda6b6cf9d9e835aac
vveggalam25 <vveggalam25@mhrd.org>
2022-10-11 11:48:56 -0400

    oct 11th

--- String_Testing.java
+++ String_Testing.java
@@ -1,8 +1,8 @@
 public class String_Testing {
 
     public static void main(String[] args) {
-        String test1 = "hi";
-        String test2 = new String("hi");
-        System.out.println(test1.equals(test2));
+        for (int i = 0; i < 5; i++){
+            System.out.println(i);
+        }
     }
 }
